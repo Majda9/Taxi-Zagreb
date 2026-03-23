03bdc40ad2e9ecb3986bd8041878238fad104ede
Majda <you@example.com>
2026-03-23 09:20:35 +0100

    fix serve sitemap directly from middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -9,14 +9,84 @@ const redirects = {
   '/en/services-prices/': '/en/services/',
 };
 
+const SITEMAP = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+    <url>
+        <loc>https://taxi-zagreb.com/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/"/>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/en/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/"/>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/usluge/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/usluge/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/services/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/usluge/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/en/services/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/usluge/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/services/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/usluge/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/o-nama/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/o-nama/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/about/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/o-nama/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/en/about/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/o-nama/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/about/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/o-nama/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/kontakti-i-rezervacije/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/kontakti-i-rezervacije/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/contact/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/kontakti-i-rezervacije/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>https://taxi-zagreb.com/en/contact/</loc>
+        <xhtml:link rel="alternate" hreflang="hr" href="https://taxi-zagreb.com/kontakti-i-rezervacije/"/>
+        <xhtml:link rel="alternate" hreflang="en" href="https://taxi-zagreb.com/en/contact/"/>
+        <xhtml:link rel="alternate" hreflang="x-default" href="https://taxi-zagreb.com/kontakti-i-rezervacije/"/>
+        <changefreq>monthly</changefreq>
+        <priority>0.9</priority>
+    </url>
+</urlset>`;
+
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const path = url.pathname.toLowerCase();
 
-  // Serve static files directly without SPA fallback
-  if (path.match(/\.(xml|txt|ico|png|jpg|webp|svg|css|js|woff2)$/)) {
-    const response = await context.next();
-    if (response.status === 200) return response;
+  // Serve sitemap.xml directly
+  if (path === '/sitemap.xml') {
+    return new Response(SITEMAP, {
+      headers: { 'Content-Type': 'application/xml; charset=utf-8' }
+    });
   }
 
   const target = redirects[path];
